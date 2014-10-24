af17efbc20a692ff3949f45d6d8fa89ad0789c47
Benjamin Bengfort <benjamin@bengfort.com>
2014-10-24 09:00:22 -0400

    response models

--- kyudo/fugato/migrations/0001_initial.py
+++ kyudo/fugato/migrations/0001_initial.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import model_utils.fields
 import django.utils.timezone
 from django.conf import settings
-import model_utils.fields
 
 
 class Migration(migrations.Migration):
@@ -15,6 +15,37 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Answer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('text', models.TextField()),
+                ('author', models.ForeignKey(related_name=b'answers', to=settings.AUTH_USER_MODEL)),
+                ('question', models.ForeignKey(related_name=b'answers', to='fugato.Answer')),
+                ('related', models.ManyToManyField(related_name='related_rel_+', to='fugato.Answer')),
+            ],
+            options={
+                'db_table': 'answers',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AnswerVote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('vote', models.SmallIntegerField(choices=[(-1, b'downvote'), (1, b'upvote')])),
+                ('answer', models.ForeignKey(related_name=b'votes', to='fugato.Answer')),
+                ('user', models.ForeignKey(related_name=b'answer_votes', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'db_table': 'answer_voting',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Question',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -22,12 +53,39 @@ class Migration(migrations.Migration):
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
                 ('text', models.CharField(max_length=512)),
                 ('hash', models.CharField(unique=True, max_length=28)),
+                ('author', models.ForeignKey(related_name=b'questions', to=settings.AUTH_USER_MODEL)),
                 ('related', models.ManyToManyField(related_name='related_rel_+', to='fugato.Question')),
-                ('users', models.ManyToManyField(related_name=b'questions', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'questions',
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='QuestionVote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('vote', models.SmallIntegerField(choices=[(-1, b'downvote'), (1, b'upvote')])),
+                ('question', models.ForeignKey(related_name=b'votes', to='fugato.Question')),
+                ('user', models.ForeignKey(related_name=b'question_votes', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'db_table': 'question_voting',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='voters',
+            field=models.ManyToManyField(related_name=b'+', through='fugato.QuestionVote', to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='answer',
+            name='voters',
+            field=models.ManyToManyField(related_name=b'+', through='fugato.AnswerVote', to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
         ),
     ]
--- kyudo/fugato/models.py
+++ kyudo/fugato/models.py
@@ -18,21 +18,23 @@ Models for the fugato app.
 ##########################################################################
 
 from django.db import models
+from model_utils import Choices
 from kyudo.utils import signature
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
 from model_utils.models import TimeStampedModel
 
 ##########################################################################
-## Models
+## Qustion and Answer Models
 ##########################################################################
 
 class Question(TimeStampedModel):
 
-    text    = models.CharField( max_length=512, null=False )  # The text of the question
-    hash    = models.CharField( max_length=28, unique=True )  # The normalized signature
-    related = models.ManyToManyField( 'self' )                # Links between related questions
-    users   = models.ManyToManyField( 'auth.User', related_name="questions")
+    text     = models.CharField( max_length=512, null=False )  # The text of the question
+    hash     = models.CharField( max_length=28, unique=True )  # The normalized signature
+    related  = models.ManyToManyField( 'self' )                # Links between related questions
+    author   = models.ForeignKey( 'auth.User', related_name="questions" )
+    voters   = models.ManyToManyField( 'auth.User', through='fugato.QuestionVote', related_name="+")
 
     class Meta:
         db_table = "questions"
@@ -40,6 +42,47 @@ class Question(TimeStampedModel):
     def __unicode__(self):
         return self.text
 
+class Answer(TimeStampedModel):
+
+    text     = models.TextField( )                             # The text of the answer
+    related  = models.ManyToManyField( 'self' )                # Links between related responses
+    author   = models.ForeignKey( 'auth.User', related_name="answers" )
+    question = models.ForeignKey( 'fugato.Answer', related_name="answers" )
+    voters   = models.ManyToManyField( 'auth.User', through='fugato.AnswerVote', related_name="+")
+
+    class Meta:
+        db_table = "answers"
+
+##########################################################################
+## Models for Up/Down voting
+##########################################################################
+
+class Voting(TimeStampedModel):
+    """
+    Abstract Voting model for up/down voting
+    """
+
+    BALLOT   = Choices((-1, 'downvote', 'downvote'), (1, 'upvote', 'upvote'))
+    vote     = models.SmallIntegerField( choices=BALLOT )
+
+    class Meta:
+        abstract = True
+
+class AnswerVote(Voting):
+
+    answer   = models.ForeignKey( 'fugato.Answer', related_name='votes' )
+    user     = models.ForeignKey( 'auth.User', related_name='answer_votes' )
+
+    class Meta:
+        db_table = "answer_voting"
+
+class QuestionVote(Voting):
+
+    question = models.ForeignKey( 'fugato.Question', related_name='votes' )
+    user     = models.ForeignKey( 'auth.User', related_name='question_votes' )
+
+    class Meta:
+        db_table = "question_voting"
 
 ##########################################################################
 ## Signals
