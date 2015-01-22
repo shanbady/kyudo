6e5fee5cbdaf0112e9f012f944c041f567424a43
Benjamin Bengfort <benjamin@bengfort.com>
2015-01-22 17:26:17 -0500

    some basic tests

--- kyudo/kyudo/tests.py
+++ kyudo/kyudo/tests.py
@@ -19,6 +19,7 @@ Tests for the Kyudo library
 
 import kyudo
 
+from kyudo.utils import *
 from unittest import TestCase
 # from django.test import TestCase
 
@@ -48,3 +49,55 @@ class InitializationTests(TestCase):
         Assert that test and package versions match
         """
         self.assertEqual(EXPECTED_VERSION, kyudo.get_version())
+
+class UtilsTests(TestCase):
+    """
+    Test the kyudo utilities library
+    """
+
+    def test_normalize(self):
+        """
+        Test the normalization function
+        """
+
+        self.assertNotIn(" ", normalize("a b c d e f     g"), "should not contain spaces")
+        self.assertNotIn("A", normalize("AAAAA AAA AA 9AA8"), "should not contain uppercase")
+        self.assertNotIn(".", normalize("no.punctuation."), "should not contain punctuation")
+        self.assertNotIn("-", normalize("no-punctuation-"), "should not contain punctuation")
+
+    def test_normalize_question(self):
+        """
+        Test question normalization
+        """
+
+        testa = "Who is faster, a T-Rex or a Velociraptor?"
+        testb = "who is faster? A t-rex or a velociraptor?"
+
+        self.assertEqual(normalize(testa), normalize(testb))
+
+    def test_signature(self):
+        """
+        Test the text signature method
+        """
+
+        self.assertEqual(len(signature("here I am")), 28, "should be base64 encoded SHA1 hash length")
+        self.assertEqual(signature("the rain in spain"), "QKv9wgxE3wSgRQevr3h1S0cg468=", "should compute the correct SHA1 hash")
+
+    def test_question_signature(self):
+        """
+        Test questions with same signature
+        """
+
+        testa = "Who is faster, a T-Rex or a Velociraptor?"
+        testb = "who is faster? A t-rex or a velociraptor?"
+
+        self.assertEqual(signature(testa), signature(testb))
+
+    def test_htmlize(self):
+        """
+        Test the htmlize function
+        """
+
+        self.assertEqual(htmlize("http://www.google.com/"), '<p><a href="http://www.google.com/" rel="nofollow">http://www.google.com/</a></p>', "linkify didn't work")
+        self.assertNotIn("<script>", htmlize("<script>alert('bad');</script>"), "clean didn't work")
+        self.assertIn("<ul>", htmlize("- item 1\n- item 2\n"), "markdown didn't work")
--- kyudo/users/tests.py
+++ kyudo/users/tests.py
@@ -1,3 +1,154 @@
-from django.test import TestCase
+# users.tests
+# Tests for the users app
+#
+# Author:   Benjamin Bengfort <bengfort@cs.umd.edu>
+# Created:  Thu Jan 22 16:47:20 2015 -0500
+#
+# Copyright (C) 2015 University of Maryland
+# For license information, see LICENSE.txt
+#
+# ID: tests.py [] bengfort@cs.umd.edu $
 
-# Create your tests here.
+"""
+Tests for the users app
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+import hashlib
+
+from users.models import Profile
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+##########################################################################
+## User Fixture
+##########################################################################
+
+fixtures = {
+    'user': {
+        'username': 'jdoe',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': 'jdoe@example.com',
+        'password': 'supersecret',
+    }
+}
+
+##########################################################################
+## Model Tests
+##########################################################################
+
+class ProfileModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(**fixtures['user'])
+
+    def test_profile_on_create(self):
+        """
+        Test the User post_save signal to create a profile
+        """
+
+        self.assertEqual(Profile.objects.count(), 1, "begin profile count mismatch (user mock has no profile?)")
+        u = User.objects.create_user(username="test", email="test@example.com", password="password")
+        self.assertEqual(Profile.objects.count(), 2, "additional profile object doesn't exist")
+        self.assertIsNotNone(u.profile)
+
+    def test_profile_email_hash_md5(self):
+        """
+        Ensure that the email_hash on a user is an MD5 digest
+        """
+
+        email   = "Jane.Doe@gmail.com"
+        udigest = hashlib.md5(email).hexdigest()
+        ldigest = hashlib.md5(email.lower()).hexdigest()
+
+        u = User.objects.create_user(username="test", email=email, password="password")
+        self.assertIsNotNone(u.profile, "user has no profile?")
+        self.assertIsNotNone(u.profile.email_hash, "user has no email hash?")
+
+        self.assertNotEqual(udigest, u.profile.email_hash, "email was not lower case before digest")
+        self.assertEqual(ldigest, u.profile.email_hash, "email not hashed correctly")
+
+    def test_profile_email_hash_create(self):
+        """
+        Email should be hashed on user create
+        """
+
+        digest = hashlib.md5(fixtures['user']['email']).hexdigest()
+
+        self.assertIsNotNone(self.user.profile, "user has no profile?")
+        self.assertIsNotNone(self.user.profile.email_hash, "user has no email hash?")
+        self.assertEqual(digest, self.user.profile.email_hash, "email hash does not match expected")
+
+    def test_profile_email_hash_update(self):
+        """
+        Email should be hashed on user update
+        """
+
+        newemail = "john.doe@gmail.com"
+        digest   = hashlib.md5(newemail).hexdigest()
+
+        self.user.email = newemail
+        self.user.save()
+
+        self.assertEqual(digest, self.user.profile.email_hash, "email hash does not match expected")
+
+##########################################################################
+## View Tests
+##########################################################################
+
+class UserViewsTest(TestCase):
+
+    def setUp(self):
+        self.user   = User.objects.create_user(**fixtures['user'])
+        self.client = Client()
+
+    def login(self):
+        credentials = {
+            'username': fixtures['user']['username'],
+            'password': fixtures['user']['password'],
+        }
+
+        return self.client.login(**credentials)
+
+    def logout(self):
+        return self.client.logout();
+
+    def test_profile_view_auth(self):
+        """
+        Assert that profile can only be viewed if logged in.
+        """
+        endpoint = reverse('profile')
+        loginurl = reverse('social:begin', args=('google-oauth2',))
+        params   = "next=%s" % endpoint
+        expected = "%s?%s" % (loginurl, params)
+        response = self.client.get(endpoint)
+
+        self.assertRedirects(response, expected, fetch_redirect_response=False)
+
+    def test_profile_object(self):
+        """
+        Assert the profile gets the current user
+        """
+
+        endpoint = reverse('profile')
+
+        self.login()
+        response = self.client.get(endpoint)
+
+        self.assertEqual(self.user, response.context['user'])
+
+    def test_profile_template(self):
+        """
+        Check that the right template is being used
+        """
+        endpoint = reverse('profile')
+
+        self.login()
+        response = self.client.get(endpoint)
+
+        self.assertTemplateUsed(response, 'registration/profile.html')
