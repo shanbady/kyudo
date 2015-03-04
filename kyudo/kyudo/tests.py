9f102053901c4a0893f21f0422db19de4bff12c3
Benjamin Bengfort <benjamin@bengfort.com>
2015-03-04 10:59:12 -0500

    bumped version to 1.0.4

--- kyudo/kyudo/tests.py
+++ kyudo/kyudo/tests.py
@@ -27,7 +27,7 @@ from unittest import TestCase
 ## Module variables
 ##########################################################################
 
-EXPECTED_VERSION = "1.0.3"
+EXPECTED_VERSION = "1.0.4"
 
 ##########################################################################
 ## Initialization Tests
